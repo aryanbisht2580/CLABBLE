91314eb3b437f374f068d90cb719a31b95ad0c10
aryanbisht2580 <aryanbisht2580@gmail.com>
2024-08-04 20:36:21 +0530

    resolving loading

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import { FcGoogle } from "react-icons/fc";
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
@@ -10,6 +10,7 @@ import Index from '.';
 import { useDispatch, useSelector } from 'react-redux';
 import { authActions, authSelector } from '@/redux/slices/authSlice';
 import { io } from 'socket.io-client';
+import { BeatLoader } from 'react-spinners';
 
 
 
@@ -18,19 +19,21 @@ const Login = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const user=useSelector(authSelector);
-
+  const [isloading,setIsLoading]=useState(false);
   useEffect(()=>{
     if(user.id && !user.newuser){
       router.push("/")
     }
   },[user])
   const handler = async () => {
+    
 
     const provider = new GoogleAuthProvider();
     const { user: userx } = await signInWithPopup(firebaseAuth, provider);
     const user = { name: userx.displayName, email: userx.email };
 
     // const user = { name: userx.displayName,image:userx.photoURL, email: userx.email };
+    setIsLoading(true);
     try {
       const { data } = await axios.post(CHECK_USER_ROUTE, {
         email: user.email
@@ -46,10 +49,15 @@ const Login = () => {
       }
     } catch (err) {
       console.log("err: " + err);
+      setIsLoading(false);
     }
+    setIsLoading(false)
 
   }
   return (
+    isloading?<div style={{height:"100vh",width:"100vw",}} className='flex justify-center items-center bg-login-bg'>
+      <BeatLoader/>
+    </div>:
     <Index>
 
 
